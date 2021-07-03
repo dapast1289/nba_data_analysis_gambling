60d030813e75589630f56233ee66dfa53e9cf406
leochen <leo@funpodium.net>
2021-07-03 15:19:59 +0800

    update

--- calculate.py
+++ calculate.py
@@ -75,7 +75,8 @@ def actually_analysis(combination_list, lose_key, continue_lose_num):
 
 
 # 隨機取樣法
-def randon_result_analysis(game_result_list, lose_key, continue_lose_num, randon_sample_count, uuid, consider_ignore_game=False):
+def randon_result_analysis(game_result_list, lose_key, continue_lose_num, randon_sample_count, thread_id, sub_thread_id,
+						   consider_ignore_game=False):
 	# print(uuid, randon_sample_count)
 	def is_add_or_odd(game_result):
 		total_pts = game_result.visitor_total_pts + game_result.home_total_pts
@@ -108,7 +109,8 @@ def randon_result_analysis(game_result_list, lose_key, continue_lose_num, randon
 	record = AnalysisRecord()
 	record.season = game_result_list[0][0].season
 	record.period_days = len(game_result_list)
-	record.uuid = uuid
+	record.thread_id = thread_id
+	record.sub_thread_id = sub_thread_id
 	record.lose_keyword = lose_key
 	record.continue_lose_num = continue_lose_num
 	record.sample_count = randon_sample_count
@@ -116,9 +118,7 @@ def randon_result_analysis(game_result_list, lose_key, continue_lose_num, randon
 	record.sample_end_date = game_result_list[len(game_result_list)-1][0].game_start_time.date()
 	record.win_count = win_count
 	record.lose_count = lose_count
-	# record.win_percent = str(win_count / (win_count + lose_count) * 100)
 	record.win_percent = win_count / (win_count + lose_count) * 100
-	# record.lose_percent = str(lose_count / (win_count + lose_count) * 100)
 	record.lose_percent = lose_count / (win_count + lose_count) * 100
 	record.cost_of_seconds = end_time - start_time
 	return record
@@ -148,9 +148,9 @@ def test_actually(game_result_list):
 	print(f"{actually_randon_combination_test_end - actually_randon_combination_test_start} 秒計算排列組合")
 
 
-def test_randon_case_by_multi_thread(game_result_list, lose_keyword, continue_lose_num, sample_count):
+def test_randon_case_by_multi_thread(thread_id, game_result_list, lose_keyword, continue_lose_num, sample_count):
 	repository = AnalysisRecordRepository()
-	max_threads = 1000
+	max_threads = 100
 	sample_per_work = []
 	divide_sample = sample_count // max_threads
 	remain_sample = sample_count % max_threads
@@ -166,22 +166,26 @@ def test_randon_case_by_multi_thread(game_result_list, lose_keyword, continue_lo
 		total_time = 0
 		total_sample = 0
 		result_list = []
-		for sample in sample_per_work:
-			future = executor.submit(randon_result_analysis, game_result_list, lose_keyword,
-									 continue_lose_num, sample, uuid.uuid4())
+		for index, sample in enumerate(sample_per_work):
+			sub_thread_id = str(thread_id) + "-" + str(index)
+			future = executor.submit(randon_result_analysis, game_result_list, lose_keyword, continue_lose_num, sample, thread_id, sub_thread_id)
 			futures.append(future)
 		for future in as_completed(futures):
 			total_time += future.result().cost_of_seconds
 			total_sample += future.result().sample_count
-			result_list.append(copy.copy(future.result()))
-			repository.save(future.result())
+			result_list.append(future.result())
+			# repository.save(future.result())
 		thread_end_time = time.time()
-		print("thread_time", thread_end_time - thread_start_time)
-		print("total_time", total_time)
-		print("total_time", total_sample)
 		for result in result_list:
-			print(type(result.lose_percent))
-			print(str(result))
+			result.cost_of_seconds_of_thread = thread_end_time - thread_start_time
+			result.sample_count_of_thread = total_sample
+		repository.save_all(result_list)
+		# print("thread_time", thread_end_time - thread_start_time)
+		# print("total_time", total_time)
+		# print("total_time", total_sample)
+		# for result in result_list:
+		# 	print(type(result.lose_percent))
+		# 	print(str(result))
 
 repository.analysis_record_repository.drop_db()
 repository.analysis_record_repository.init_db()
@@ -193,12 +197,13 @@ game_result_list_2019 = repository.query_from_statement("2019")
 game_result_list_2020 = repository.query_from_statement("2020")
 game_result_list_2021 = repository.query_from_statement("2021")
 
-for x in range(1000):
-	test_randon_case_by_multi_thread(game_result_list_2018, "雙", 8, 10000000)
-	test_randon_case_by_multi_thread(game_result_list_2019, "雙", 8, 10000000)
-	test_randon_case_by_multi_thread(game_result_list_2020, "雙", 8, 10000000)
-	test_randon_case_by_multi_thread(game_result_list_2021, "雙", 8, 10000000)
-	test_randon_case_by_multi_thread(game_result_list_2018, "單", 8, 10000000)
-	test_randon_case_by_multi_thread(game_result_list_2019, "單", 8, 10000000)
-	test_randon_case_by_multi_thread(game_result_list_2020, "單", 8, 10000000)
-	test_randon_case_by_multi_thread(game_result_list_2021, "單", 8, 10000000)
+sample_num = 100000
+for x in range(1):
+	test_randon_case_by_multi_thread(uuid.uuid4(), game_result_list_2018, "雙", 8, sample_num)
+	test_randon_case_by_multi_thread(uuid.uuid4(), game_result_list_2019, "雙", 8, sample_num)
+	test_randon_case_by_multi_thread(uuid.uuid4(), game_result_list_2020, "雙", 8, sample_num)
+	test_randon_case_by_multi_thread(uuid.uuid4(), game_result_list_2021, "雙", 8, sample_num)
+	test_randon_case_by_multi_thread(uuid.uuid4(), game_result_list_2018, "單", 8, sample_num)
+	test_randon_case_by_multi_thread(uuid.uuid4(), game_result_list_2019, "單", 8, sample_num)
+	test_randon_case_by_multi_thread(uuid.uuid4(), game_result_list_2020, "單", 8, sample_num)
+	test_randon_case_by_multi_thread(uuid.uuid4(), game_result_list_2021, "單", 8, sample_num)
